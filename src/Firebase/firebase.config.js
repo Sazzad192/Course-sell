31aff66d5c76fdd81e955b38c26670d39b9f10a6
K.M. SAZZADUL ISLAM <18-37693-1@student.aiub.edu>
2022-10-27 04:48:33 +0600

    Environment Variable update

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBU5EQDCGfYMGWjHjvkKCVY7zZICst8eU4",
-  authDomain: "learning-a0d07.firebaseapp.com",
-  projectId: "learning-a0d07",
-  storageBucket: "learning-a0d07.appspot.com",
-  messagingSenderId: "127011655584",
-  appId: "1:127011655584:web:ce7792c18708f1e36a5ec5"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
